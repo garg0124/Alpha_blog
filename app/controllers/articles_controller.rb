74e5c4c9bcc4a43cbad6d89113b184c85d9cfd69
Manasvi Garg <manasvi.garg@unthinkable.co>
2021-11-25 17:53:11 +0530

    password authentication checked

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,6 +20,8 @@ class ArticlesController < ApplicationController
     # render plain: params[:article]       for rendering data on browser
     # to save data into database
        @article = Article.new(article_params)
+       @article.user = User.first
+       puts(@article.user.id)
        if @article.save
           flash[:notice] = "Article was created successfully"
        redirect_to @article
